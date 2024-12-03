da45168b616cb7de506244cdfa2cd559c234e964
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-12-03 11:57:05 +0100

    feat: with custom required

--- src/examples/03-typeof-keyof/02-keyof.ts
+++ src/examples/03-typeof-keyof/02-keyof.ts
@@ -1,21 +1,39 @@
+// interface User {
+//   name: string;
+//   age: number;
+//   email: string;
+// }
+
+// type UserKeys = keyof User; // "name" | "age" | "email"
+// const key: UserKeys = 'age';
+
 interface User {
   name: string;
   age: number;
-  email: string;
-}
-
-type UserKeys = keyof User; // "name" | "age" | "email"
-const key: UserKeys = 'age';
-
-interface User {
-  name: string;
-  age: number;
-  email: string;
+  email?: string;
+  // email: string | undefined;
 }
 
 type PartialUser = Partial<User>;
 // type { name?: string; age?: number}
-type MyPartialUser<T> = {
+type CustomPartialUser<T> = {
   [K in keyof T]?: T[K];
 };
-type PartialUser2 = MyPartialUser<User>; // type { name?: string; age?: number}
+type PartialUser2 = CustomPartialUser<User>; // type { name?: string; age?: number}
+
+interface Config {
+  host?: string;
+  port?: number;
+}
+
+interface Config2 {
+  host: string;
+  port?: number;
+}
+
+type RequiredConfig = Required<Config>;
+type CustomRequiredConfig<T> = {
+  [K in keyof T]-?: T[K];
+};
+
+type Config3 = CustomRequiredConfig<Config2>;
